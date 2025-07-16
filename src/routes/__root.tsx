b516ba3d531e1907a7d5d2530bf0e5bc96c55705
Adam Prchal <a.prchal@proton.me>
2025-07-16 23:23:08 +0200

    feat: first poc reciple list page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,51 +1,53 @@
 /// <reference types="vite/client" />
 import type { ReactNode } from "react";
 import {
-  Outlet,
-  createRootRoute,
-  HeadContent,
-  Scripts,
+    Outlet,
+    createRootRoute,
+    HeadContent,
+    Scripts,
 } from "@tanstack/react-router";
 
 import appCss from "../styles/app.css?url";
+import { Navigation } from "~/components/Navigation";
 
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      {
-        title: "Mlsano",
-      },
-    ],
-    links: [{ rel: "stylesheet", href: appCss }],
-  }),
-  component: RootComponent,
+    head: () => ({
+        meta: [
+            {
+                charSet: "utf-8",
+            },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+            {
+                title: "Mlsano",
+            },
+        ],
+        links: [{ rel: "stylesheet", href: appCss }],
+    }),
+    component: RootComponent,
 });
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  );
+    return (
+        <RootDocument>
+            <Outlet />
+        </RootDocument>
+    );
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  return (
-    <html data-theme="bumblebee">
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  );
+    return (
+        <html data-theme="forest">
+            <head>
+                <HeadContent />
+            </head>
+            <body>
+                {children}
+                <Navigation />
+                <Scripts />
+            </body>
+        </html>
+    );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,49 +1,72 @@
 // src/routes/index.tsx
-import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
+import { db } from "~/db";
+import { Link } from "@tanstack/react-router";
 
-const filePath = "count.txt";
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, "utf-8").catch(() => "0"),
-  );
-}
-
-const getCount = createServerFn({
-  method: "GET",
-}).handler(() => {
-  return readCount();
+const getRecipes = createServerFn({
+    method: "GET",
+}).handler(async () => {
+    return await db.query.recipes.findMany({
+        with: {
+            recipeTags: {
+                with: {
+                    tag: true,
+                },
+            },
+        },
+    });
 });
 
-const updateCount = createServerFn({ method: "POST" })
-  .validator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount();
-    await fs.promises.writeFile(filePath, `${count + data}`);
-  });
-
 export const Route = createFileRoute("/")({
-  component: Home,
-  loader: async () => await getCount(),
+    component: Home,
+    loader: () => getRecipes(),
 });
 
 function Home() {
-  const router = useRouter();
-  const state = Route.useLoaderData();
+    const router = useRouter();
+    const recipes = Route.useLoaderData();
 
-  return (
-    <button
-      className="btn btn-primary"
-      type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate();
-        });
-      }}
-    >
-      Add 1 to {state}?
-    </button>
-  );
+    console.log(recipes);
+    return (
+        <main className="mx-auto max-w-4xl p-2 pb-32">
+            <hgroup className="prose mt-4 mb-6 px-4">
+                <h1>Recepty</h1>
+            </hgroup>
+            <ul className="list bg-base-100 rounded-box shadow-md ">
+                {recipes.map((recipe, index) => (
+                    <li>
+                        <Link className="list-row hover:bg-neutral" to={"/"}>
+                            <div className="text-4xl font-thin opacity-30 tabular-nums">
+                                {index < 10 && "0"}
+                                {index < 100 && "0"}
+                                {index}
+                            </div>
+                            <img
+                                className="size-24 rounded-box object-cover"
+                                src={recipe.image_url || ""}
+                            />
+                            <article className="list-col-grow justify-between flex flex-col">
+                                <hgroup className="mb-2">
+                                    <h3 className="text-lg font-bold">
+                                        {recipe.title}
+                                    </h3>
+                                    <p className="text-xs">
+                                        {recipe.cook_time || "10 min"}
+                                    </p>
+                                </hgroup>
+                                <div className="text-xs uppercase font-semibold opacity-60">
+                                    {recipe.recipeTags.map((t) => (
+                                        <div className="badge badge-soft ">
+                                            {t.tag?.name}
+                                        </div>
+                                    ))}
+                                </div>
+                            </article>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
