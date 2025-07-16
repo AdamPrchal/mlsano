fadfd5a7e82b82e8759e2dd3affad532b122f4da
Adam Prchal <a.prchal@proton.me>
2025-07-16 23:22:33 +0200

    fix: set correct drizzle DB relations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 
 export const recipes = sqliteTable("recipes", {
     id: int().primaryKey({ autoIncrement: true }),
@@ -22,7 +23,7 @@ export const recipeIngredients = sqliteTable("recipe_ingredients", {
     recipeId: int().references(() => recipes.id, {
         onDelete: "cascade",
     }),
-    ingredientId: int().references(() => recipes.id, {
+    ingredientId: int().references(() => ingredients.id, {
         onDelete: "set null",
     }),
     quantity: int().notNull(),
@@ -41,3 +42,42 @@ export const recipeTags = sqliteTable("recipe_tags", {
     }),
     tagId: int().references(() => tags.id),
 });
+
+// Relations
+export const recipesRelations = relations(recipes, ({ many }) => ({
+    recipeIngredients: many(recipeIngredients),
+    recipeTags: many(recipeTags),
+}));
+
+export const ingredientsRelations = relations(ingredients, ({ many }) => ({
+    recipeIngredients: many(recipeIngredients),
+}));
+
+export const recipeIngredientsRelations = relations(
+    recipeIngredients,
+    ({ one }) => ({
+        recipe: one(recipes, {
+            fields: [recipeIngredients.recipeId],
+            references: [recipes.id],
+        }),
+        ingredient: one(ingredients, {
+            fields: [recipeIngredients.ingredientId],
+            references: [ingredients.id],
+        }),
+    }),
+);
+
+export const tagsRelations = relations(tags, ({ many }) => ({
+    recipeTags: many(recipeTags),
+}));
+
+export const recipeTagsRelations = relations(recipeTags, ({ one }) => ({
+    recipe: one(recipes, {
+        fields: [recipeTags.recipeId],
+        references: [recipes.id],
+    }),
+    tag: one(tags, {
+        fields: [recipeTags.tagId],
+        references: [tags.id],
+    }),
+}));
